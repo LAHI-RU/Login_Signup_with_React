13f5890f3210a3535a7d95c639100a054d35ce01
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-06-24 17:44:22 +0530

    signup part

---
+++ client/src/Components/Signup.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "../App.css";
+import Axios from "axios";
+
+const Signup = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    Axios.post("http://localhost:3000/auth/signup", {
+      username,
+      email,
+      password,
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className="sign-up-container">
+      <form className="sign-up-form" onSubmit={handleSubmit}>
+        <h2>Sign Up</h2>
+        <label htmlFor="username"> Username: </label>
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <label htmlFor="email"> Email: </label>
+        <input
+          type="email"
+          autoComplete="off"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label htmlFor="password"> Password: </label>
+        <input
+          type="password"
+          placeholder="******"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit"> Sign Up </button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
